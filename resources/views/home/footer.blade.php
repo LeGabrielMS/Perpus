402d41257e405e98ee517247aa695310d5a8a2d3
LeGabrielMS <gabrielmarcellino77@gmail.com>
2025-05-08 02:46:50 +0700

    Update README and footer links to point to the correct GitHub repository

--- resources/views/home/footer.blade.php
+++ resources/views/home/footer.blade.php
@@ -4,7 +4,7 @@
          <div class="row">
              <div class="col-lg-12">
                  <p>2025 &copy; Universitas Teknologi Bandung. Source Project: <a target="_blank"
-                         href="https://github.com/your-repo-link">GitHub Repository</a>.</p>
+                         href="https://github.com/LeGabrielMS/Perpus.git">GitHub Repository</a>.</p>
                  <p>Designed by <a title="HTML CSS Templates">Gabriel Marcellino Sinurat - 22552011043</a></p>
 
              </div>
